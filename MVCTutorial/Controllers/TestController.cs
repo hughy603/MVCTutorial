af27a5beca4870fa5d74c192499f088eed44c25b
hughy603 <rice.adam@gmail.com>
2017-02-26 01:24:37 -0500

    Created model and passed data between controller and view

--- MVCTutorial/Controllers/TestController.cs
+++ MVCTutorial/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCTutorial.Models;
 
 namespace MVCTutorial.Controllers
 {
@@ -15,6 +16,11 @@ namespace MVCTutorial.Controllers
 
         public ActionResult GetView()
         {
+            Employee emp = new Employee();
+            emp.FirstName = "Adam";
+            emp.LastName = "Rice";
+            emp.Salary = 100;
+            ViewData["Employee"] = emp;
             return View("MyView");
         }
     }
